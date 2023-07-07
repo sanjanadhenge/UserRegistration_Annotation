fac79f93f7ec98a92ec4aba015b71321e07491a0
sanjanadhenge <dhengesanjana2001@gmail.com>
2023-07-07 17:38:59 +0530

    [sanjana] Add : First Name

--- Annotation_User_Registration/Program.cs
+++ Annotation_User_Registration/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Annotation_User_Registration
+{
+    internal class Program
+    {
+        
+        public static void Main(string[] args) 
+        {
+           User_Reg user_Reg = new User_Reg();
+            user_Reg.FirstName = "Sanjana";
+            ValidationContext context = new ValidationContext(user_Reg, null, null);
+            List<ValidationResult> results = new List<ValidationResult>();
+            bool valid = Validator.TryValidateObject(user_Reg, context, results, true);
+            if (!valid)
+            {
+                foreach (ValidationResult result in results)
+                {
+                    Console.WriteLine("Member Name :{0}", result.MemberNames.First(), Environment.NewLine);
+                    Console.WriteLine("Error Message :: {0}{1}", result.ErrorMessage, Environment.NewLine);
+                }
+            }
+            else
+            {
+                Console.WriteLine(user_Reg.FirstName);
+            }
+        }
+    }
+}
--- Annotation_User_Registration/User_Reg.cs
+++ Annotation_User_Registration/User_Reg.cs
@@ -1,12 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Annotation_User_Registration
 {
-    internal class Contact
+    internal class User_Reg
     {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Employee {0} is required")]
+        [StringLength(100, MinimumLength = 3, ErrorMessage = "Name should be minimum 3 characters and maximum of 100 characters")]
+        [DataType(DataType.Text)]
+        public string FirstName { get; set; }
+       
     }
 }
